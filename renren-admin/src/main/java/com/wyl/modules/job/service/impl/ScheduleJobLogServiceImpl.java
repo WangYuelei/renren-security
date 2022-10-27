02009328bee441e1ee5591c24a9aa9c3a7a19320
wangyuelei <84227396@qq.com>
2022-10-27 17:13:55 +0800

    first

---
+++ renren-admin/src/main/java/com/wyl/modules/job/service/impl/ScheduleJobLogServiceImpl.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2018 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.wyl.modules.job.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.wyl.common.constant.Constant;
+import com.wyl.common.page.PageData;
+import com.wyl.common.service.impl.BaseServiceImpl;
+import com.wyl.common.utils.ConvertUtils;
+import com.wyl.modules.job.dao.ScheduleJobLogDao;
+import com.wyl.modules.job.dto.ScheduleJobLogDTO;
+import com.wyl.modules.job.entity.ScheduleJobLogEntity;
+import com.wyl.modules.job.service.ScheduleJobLogService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ScheduleJobLogServiceImpl extends BaseServiceImpl<ScheduleJobLogDao, ScheduleJobLogEntity> implements ScheduleJobLogService {
+
+	@Override
+	public PageData<ScheduleJobLogDTO> page(Map<String, Object> params) {
+		IPage<ScheduleJobLogEntity> page = baseDao.selectPage(
+			getPage(params, Constant.CREATE_DATE, false),
+			getWrapper(params)
+		);
+		return getPageData(page, ScheduleJobLogDTO.class);
+	}
+
+	private QueryWrapper<ScheduleJobLogEntity> getWrapper(Map<String, Object> params){
+		String jobId = (String)params.get("jobId");
+
+		QueryWrapper<ScheduleJobLogEntity> wrapper = new QueryWrapper<>();
+		wrapper.eq(StringUtils.isNotBlank(jobId), "job_id", jobId);
+
+		return wrapper;
+	}
+
+	@Override
+	public ScheduleJobLogDTO get(Long id) {
+		ScheduleJobLogEntity entity = baseDao.selectById(id);
+
+		return ConvertUtils.sourceToTarget(entity, ScheduleJobLogDTO.class);
+	}
+
+}
---
+++ renren-admin/src/main/java/com/wyl/modules/sys/dao/SysDictTypeDao.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2018 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.wyl.modules.sys.dao;
+
+import com.wyl.common.dao.BaseDao;
+import com.wyl.modules.sys.entity.DictType;
+import com.wyl.modules.sys.entity.SysDictTypeEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 字典类型
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Mapper
+public interface SysDictTypeDao extends BaseDao<SysDictTypeEntity> {
+
+    /**
+     * 字典类型列表
+     */
+    List<DictType> getDictTypeList();
+
+}
